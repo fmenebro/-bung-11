1cd8305e4bc9301166781c59017e70351909663d
fmenebro <f.menebroecker@smail.uni-koeln.de>
2026-01-16 20:41:31 +0100

    Create H1_main.java

---
+++ h1/H1_main.java
@@ -0,0 +1,21 @@
+package h1;
+
+public class H1_main {
+
+    public static int distance(Node x, Node y) {
+        if (x == y) {
+            return 0;
+        }
+        return 1 + distance(x.getNext(), y);
+    }
+
+    public static void main(String[] args) {
+        Node e = new Node(null);
+        Node d = new Node(e);
+        Node c = new Node(d);
+        Node b = new Node(c);
+        Node a = new Node(b);
+
+        System.out.println(distance(a, d));
+    }
+}
